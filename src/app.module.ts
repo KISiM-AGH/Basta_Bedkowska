8234b471107e1f8b0c02e1169d1b689517491ce5
BastaBasta <76259920+BastaBasta@users.noreply.github.com>
2023-01-21 12:52:51 +0100

    Adding game module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WordModule } from './modules/word/word.module';
+import { GameModule } from './modules/game/game.module';
 
 @Module({
-  imports: [WordModule],
+  imports: [WordModule, GameModule],
   controllers: [AppController],
   providers: [AppService],
 })
