5289fce5a2932701855b62009f9dbac706099128
ZuzannaBędkowska <zbedkowska@student.agh.edu.pl>
2023-01-22 21:35:52 +0100

    edycja guzika - dodanie handlera i zmiana wyglądu

--- frontend/src/pages/layouts/keyboard.tsx
+++ frontend/src/pages/layouts/keyboard.tsx
@@ -1,8 +1,22 @@
-import React, {FC} from "react";
-import {Button, Flex, Grid} from "@mantine/core";
+import React, {FC, useState} from "react";
+import {Flex, Grid} from "@mantine/core";
 import classes from "./keyboard.module.css";
+import {showNotification} from "@mantine/notifications";
 
 export const KeyboardForm: FC = () => {
+    const [clickedbutton, setClickedbutton] = useState('');
+    const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+
+        const button: HTMLButtonElement = event.currentTarget;
+        button.style.backgroundColor = 'lightgray';
+        button.setAttribute('disabled', '');
+        showNotification({
+            color: 'green',
+            title: 'button clicked: ',
+            message: button.name
+        })
+    };
     return (
         <Flex className={classes.keyboard}
             mih={50}
@@ -14,36 +28,36 @@ export const KeyboardForm: FC = () => {
             wrap="wrap"
             >
             <Grid gutter="sm" columns={6}>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>A</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>Ą</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>B</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>C</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>Ć</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>D</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>E</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>Ę</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>F</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>G</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>H</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>I</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>J</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>K</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>L</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>Ł</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>M</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>N</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>Ń</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>O</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>Ó</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>P</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>R</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>S</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>Ś</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>T</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>U</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>W</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>Y</Button></Grid.Col>
-                <Grid.Col span={1}><Button variant="light" radius="xl"  uppercase>Z</Button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"A"}>A</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"Ą"}>Ą</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"B"}>B</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"C"}>C</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"Ć"}>Ć</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"D"}>D</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"E"}>E</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"Ę"}>Ę</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"F"}>F</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"G"}>G</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"H"}>H</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"I"}>I</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"J"}>J</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"K"}>K</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"L"}>L</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"Ł"}>Ł</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"M"}>M</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"N"}>N</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"Ń"}>Ń</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"O"}>O</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"Ó"}>Ó</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"P"}>P</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"R"}>R</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"S"}>S</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"Ś"}>Ś</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"T"}>T</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"U"}>U</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"W"}>W</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"Y"}>Y</button></Grid.Col>
+                <Grid.Col span={1}><button onClick={buttonHandler} className={classes.button} name={"Z"}>Z</button></Grid.Col>
             </Grid>
         </Flex>
     );
