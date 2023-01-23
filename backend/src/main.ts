c351c4c220d7df82c636344285888114af42565d
BastaBasta <76259920+BastaBasta@users.noreply.github.com>
2023-01-23 13:52:20 +0100

    Database setup & cors handling

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import { GameState } from './modules/game/game-state';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('/api/');
   app.useGlobalPipes(
     new ValidationPipe({
