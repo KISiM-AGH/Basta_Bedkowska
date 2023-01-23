b1f8be182c8a2ba790b625d0ebce44b65f7e52b9
ZuzannaBędkowska <zbedkowska@student.agh.edu.pl>
2023-01-23 14:03:06 +0100

    bazusia naprawiona

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,7 @@ import { GameState } from './modules/game/game-state';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/');
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
